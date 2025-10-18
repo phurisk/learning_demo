12b79a987ffea5b35dc3dee15d8c90479acd14f5
phurisk <phuris.k@kkumail.com>
2025-10-18 23:48:22 +0700

    demo_web2

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,10 @@ const sarabun = Sarabun({
 })
 
 export const metadata: Metadata = {
-  title: "ฟิสิกส์ครูพี่เต้ย - เรียนฟิสิกส์อย่างเป็นระบบกับฟิสิกส์พี่เต้ย",
-  description: "เรียนฟิสิกส์กับครูพี่เต้ย ผู้เชี่ยวชาญด้านฟิสิกส์ที่มีประสบการณ์การสอนมากมาย พร้อมเทคนิคการสอนที่เข้าใจง่าย",
-  keywords: "ฟิสิกส์, กวดวิชา, ครูพี่เต้ย, เรียนฟิสิกส์, GAT, PAT, ฟิสิกส์ม.ปลาย",
-  generator: "physics-ptoey.app",
+  title: "Demo Learning Platfor",
+  description: "Demo Learning Platform ",
+  keywords: "Demo Learning Platform",
+  generator: "Demo-Learning.app",
   icons: {
     icon: "/new-logo.png",
   },
